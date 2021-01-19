75d6586ef30678fe331573bfe7c8f3f30dc3439a
Kev <kmditroia@gmail.com>
2021-01-19 15:05:37 -0500

    Update api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export function getAllItems() {
     return axios.get('https://content-hub-cdn.azureedge.net/wawa-menu-34058.json', 
-    {headers: {'Access-Control-Allow-Origin': '*'}});
+    {headers: {'Access-Control-Allow-Origin': 'https://sitecore-kiosk-demo.herokuapp.com/'}});
 }
