b43d2d4ae4c64f18ad1e141c00eccde2cc185df4
Kev <kmditroia@gmail.com>
2021-01-19 14:25:16 -0500

    build

---
+++ build/precache-manifest.ab1adb85b5d25c28eb0a1cb7e9431f82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c6374c049cd95c597715906e0ff531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcac327711907b134155",
+    "url": "/static/css/2.d079b533.chunk.css"
+  },
+  {
+    "revision": "96706f75a5f8faf8c8cb",
+    "url": "/static/css/main.f4608a45.chunk.css"
+  },
+  {
+    "revision": "bcac327711907b134155",
+    "url": "/static/js/2.9e296fbf.chunk.js"
+  },
+  {
+    "revision": "7ba7f2634383ff6ca36dc663e96d947c",
+    "url": "/static/js/2.9e296fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96706f75a5f8faf8c8cb",
+    "url": "/static/js/main.dafa12ae.chunk.js"
+  },
+  {
+    "revision": "fa6b0bd42d1bcf980242",
+    "url": "/static/js/runtime-main.b3b0a807.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
